d8e4b41a2dbba85775658e86e34798d800727657
iaroslav-sk <iaroslav.dev.sk@gmail.com>
2021-08-23 20:24:33 +0300

    Добавляет таблицу транзакций
    Добавлена таблица транзакций, предварительно спозиционированна с минимальными стилями,
    изменено позиционирование кнопки добавления траннзакции

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Header from './Header';
 import Navigation from './Navigation';
 import Currency from './Currency';
 import ContainerAppWrapper from './ContainerAppWrapper';
+import OperationsTable from './Table';
 
-export { Header, Navigation, Currency, ContainerAppWrapper };
+export { Header, Navigation, Currency, ContainerAppWrapper, OperationsTable };
