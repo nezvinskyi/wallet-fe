f081e93fb6156125d4a341035405deecda415286
Dmitry Nezvinskyi <nezvinskyi@gmail.com>
2021-08-23 17:57:23 +0200

    Update StatisticPage.jsx

--- src/pages/StatisticPage/StatisticPage.jsx
+++ src/pages/StatisticPage/StatisticPage.jsx
@@ -1,7 +1,7 @@
 import { Currency, Header, Navigation } from '../../components';
 import DiagramTab from '../../components/DiagramTab';
 
-import styles from './StatisticPage.module.css';
+// import styles from './StatisticPage.module.css';
 
 export default function StatisticPage() {
   return (
