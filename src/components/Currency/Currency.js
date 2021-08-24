df01e7ce9a66e6b12837e3cc1e29948f95d1b3ea
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-08-25 02:59:18 +0300

    Update Currency.js

--- src/components/Currency/Currency.js
+++ src/components/Currency/Currency.js
@@ -5,26 +5,28 @@ import { nanoid } from 'nanoid';
 
 export default class CurrencyList extends React.Component {
   state = {
-    currencys: []
+    currencies: [],
+  };
+  //вынеси запрос в апи (services). создай там отдельный файл
+  componentDidMount() {
+    axios.get('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11').then(res => {
+      const currencys = res.data;
+      this.setState({ currencys });
+    });
   }
 
-  componentDidMount() {
-    axios.get('https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11')
-      .then(res => {
-        const currencys = res.data;
-        this.setState({ currencys });
-      })
-  }
-  
   render() {
     return (
       <div className={styles.currency}>
-        <h3>Курс валют</h3> 
+        <h3>Курс валют</h3>
         <ul>
-          {this.state.currencys.map(currency => <li id={nanoid(10)}>{currency.ccy}, {currency.base_ccy}, {currency.buy}, {currency.sale}</li>)}
+          {this.state.currencys.map(currency => (
+            <li id={nanoid(10)}>
+              {currency.ccy}, {currency.base_ccy}, {currency.buy}, {currency.sale}
+            </li>
+          ))}
         </ul>
       </div>
-            
-    )
+    );
   }
 }
