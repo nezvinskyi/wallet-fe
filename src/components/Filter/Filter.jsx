bb31db918786adf91cdecc367265434a77e871ed
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-08-25 13:07:58 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,34 +1,49 @@
-import styles from './Filter.module.css'
-import img from './arrow.svg'
+import styles from './Filter.module.css';
+import img from './arrow.svg';
 export default function Filter() {
   const currentYear = new Date().getFullYear();
-  const years = []
+  const years = [];
   for (let i = currentYear - 5; i <= currentYear + 5; i++) {
     years.push(i);
   }
-  const months = ["Январь", "Февраль","Март","Апрель", "Май","Июнь","Июль","Август","Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+  const months = [
+    'Январь',
+    'Февраль',
+    'Март',
+    'Апрель',
+    'Май',
+    'Июнь',
+    'Июль',
+    'Август',
+    'Сентябрь',
+    'Октябрь',
+    'Ноябрь',
+    'Декабрь',
+  ];
 
   return (
-  <form className={styles.wrapper} onChange={()=>{}}>
-
-    <select name="" value='Месяц' className={styles.monthPicker} >
-  <option name="" value="Месяц" className={styles.pickerFirstOpt}>Месяц</option>
-  {months.map(month => (
+    <form className={styles.wrapper} onChange={() => {}}>
+      <select name="" value="Месяц" className={styles.monthPicker}>
+        <option name="" value="Месяц" className={styles.pickerFirstOpt}>
+          Месяц
+        </option>
+        {months.map(month => (
           <option key={month} value={months.indexOf(month)}>
             {month}
           </option>
         ))}
-</select>
+      </select>
 
-<select name="year" value='Год' className={styles.yearPicker}>
-  
-<option name="year" value="Год" className={styles.pickerFirstOpt}>Год</option>
+      <select name="year" value="Год" className={styles.yearPicker}>
+        <option name="year" value="Год" className={styles.pickerFirstOpt}>
+          Год
+        </option>
         {years.map(year => (
           <option key={year} value={year}>
             {year}
           </option>
         ))}
       </select>
-      </form>
-  )
+    </form>
+  );
 }
