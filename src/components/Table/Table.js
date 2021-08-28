5707f18b369bb688b722c97381eac92fdc742a3d
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-08-28 21:09:42 +0300

    refactor

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,33 +1,9 @@
 import moment from 'moment';
 import { Table } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
-// import transaction from './exampleData';
 import { financeSelectors } from '../../redux/finance/';
 
 import styles from './Style.module.css';
-
-// const color = [
-//   {
-//     id: '612663e26ff2885ca454662f',
-//     color: 'rgba(254, 208, 87, 1)',
-//   },
-//   {
-//     id: '6126647352796137f0f9a16b',
-//     color: 'rgba(255, 216, 208, 1)',
-//   },
-//   {
-//     id: '61278871099e747d24dd9f28',
-//     color: 'rgba(197, 186, 255, 1)',
-//   },
-//   {
-//     id: '6127c5622450ab5d00458090',
-//     color: 'rgba(110, 120, 232, 1)',
-//   },
-//   {
-//     id: '6127c5732450ab5d00458093',
-//     color: '(74, 86, 226, 1)',
-//   },
-// ];
 
 const style = {
   table: {
