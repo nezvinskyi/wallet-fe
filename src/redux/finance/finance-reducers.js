79a792172096121af19dcc31ac4cedec100350f2
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-08-30 02:27:17 +0300

    resets redux on logout

--- src/redux/finance/finance-reducers.js
+++ src/redux/finance/finance-reducers.js
@@ -1,10 +1,12 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import * as actions from './finance-actions';
+import * as userActions from '../user/user-actions';
 
 const transactions = createReducer([], {
   [actions.getTransactionsSuccess]: (_, { payload }) => payload,
   [actions.addTransactionSuccess]: (state, { payload }) => [...state, payload],
+  [userActions.logoutSuccess]: state => [],
 });
 
 const categories = createReducer([], {
@@ -12,8 +14,9 @@ const categories = createReducer([], {
   // [actions.add]: (state, { payload }) => [...state, payload],
 });
 
-const balance = createReducer([], {
+const balance = createReducer(null, {
   [actions.getBalanceSuccess]: (_, { payload }) => payload,
+  [userActions.logoutSuccess]: () => null,
 });
 
 export default combineReducers({
