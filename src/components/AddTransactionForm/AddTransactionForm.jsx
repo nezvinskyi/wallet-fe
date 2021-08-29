917cbb69503cb9ab34700faba6611d8a04e619ea
Andrii Torkotiuk <torkotiuk@gmail.com>
2021-08-29 03:33:58 +0200

    styles from AddTr form

--- src/components/AddTransactionForm/AddTransactionForm.jsx
+++ src/components/AddTransactionForm/AddTransactionForm.jsx
@@ -2,11 +2,9 @@ import moment from 'moment';
 import { Button } from 'react-bootstrap';
 import { useState, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import api from '../../service/transactions-api';
 import { globalOperations } from '../../redux/global';
 import { financeSelectors, financeOperations } from '../../redux/finance';
 import styles from './AddTransactionForm.module.css';
-import { BtnImg } from './img';
 
 const stylesJSX = {
   primaryBtn: {
@@ -45,9 +43,10 @@ const stylesJSX = {
 const AddTransactionForm = () => {
   const [date, setDate] = useState(moment(new Date()).format('YYYY-MM-DD'));
   const [type, setType] = useState(false);
-  const [amount, setAmount] = useState('0.00');
+  const [amount, setAmount] = useState('');
   const categories = useSelector(financeSelectors.getAllCategories);
-  const [categoryId, setCategoryId] = useState(categories[0]._id);
+  // const [categoryId, setCategoryId] = useState(categories[0]._id);
+  const [categoryId, setCategoryId] = useState('1111111111111');
   const [comments, setComments] = useState('');
 
   const dispatch = useDispatch();
@@ -121,38 +120,46 @@ const AddTransactionForm = () => {
           </span>
         </div>
 
+        <div className={styles.categoryWrapper}>
+          <select name="categoryId" className={styles.select} onChange={categoryChange}>
+            {categories.map(category => (
+              <option className={styles.optionFilled} key={category._id} value={category._id}>
+                {category.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
         <div className={styles.amountAndData__wrapper}>
           <input
             type="number"
             name="amount"
-            className={styles.modal__input}
+            className={styles.amountAndData}
             value={amount}
             onChange={changeAmount}
+            placeholder="0.00"
             required
           />
 
-          <input type="date" name="date" value={date} onChange={changeDate} />
+          <input
+            className={styles.amountAndData}
+            type="date"
+            name="date"
+            value={date}
+            onChange={changeDate}
+          />
         </div>
 
-        <select onChange={categoryChange} name="categoryId">
-          {categories.map(category => (
-            <option key={category._id} value={category._id}>
-              {category.name}
-            </option>
-          ))}
-        </select>
-
         <div className={styles.textareaWrapper}>
-          <textarea
+          <input
+            className={styles.text}
             name="comments"
             value={comments}
             onChange={commentsChange}
-            className={styles.modal__message}
             placeholder="Комментарий"
             required
-          ></textarea>
+          />
         </div>
-
         <div className={styles.btnGroup}>
           <Button variant="primary" type="submit" style={stylesJSX.primaryBtn}>
             ДОБАВИТЬ
