ceada0f80ec52a847b0d9caf25a20f4e610e18e4
Dmitry Nezvinskyi <Nezvinskyi@gmail.com>
2021-09-01 13:49:19 +0300

    bugfix display only expenses in stats

--- src/components/DiagramTab/statsInfo.js
+++ src/components/DiagramTab/statsInfo.js
@@ -12,66 +12,60 @@ export const getTotal = transactions => {
         total.expense = total.expense + transaction.amount;
         break;
 
-        case 'income':
+      case 'income':
         total.income = total.income + transaction.amount;
         break;
       default:
         console.log('invalid type');
     }
   });
-  
+
   return total;
 };
 
-
-export const getStats = transactions =>{
+export const getStats = transactions => {
   const notFilteredStats = [];
-  
+
   transactions.forEach(transaction => {
     const categoryId = transaction.categoryId._id;
-    
-    categoryColors.forEach(color => {
-          // if (color.id === categoryId && transaction.type === "expense") {
-          if (color.id === categoryId) {
-          notFilteredStats.push( {
-            id: transaction.categoryId._id,
-            amount: transaction.amount,
-            name: transaction.categoryId.name,
-            color: color.color,
-          }) 
-        }
-      });
-    });
-    const allStats = notFilteredStats.map(({id, amount, name, color}) => {
-      const sum = notFilteredStats 
-      .filter(item => item.id === id)
-      .reduce((total, item) => total+item.amount,0)
-      return{
-        id,
-        name,
-        amount:sum,
-        color,
-      } 
-      })
 
-    
-    const filteredStats = uniqueBy('id')
-    return filteredStats(allStats)
+    categoryColors.forEach(color => {
+      if (color.id === categoryId && transaction.type === 'expense') {
+        // if (color.id === categoryId) {
+        notFilteredStats.push({
+          id: transaction.categoryId._id,
+          amount: transaction.amount,
+          name: transaction.categoryId.name,
+          color: color.color,
+        });
+      }
+    });
+  });
+  const allStats = notFilteredStats.map(({ id, amount, name, color }) => {
+    const sum = notFilteredStats
+      .filter(item => item.id === id)
+      .reduce((total, item) => total + item.amount, 0);
+    return {
+      id,
+      name,
+      amount: sum,
+      color,
+    };
+  });
+
+  const filteredStats = uniqueBy('id');
+  return filteredStats(allStats);
 };
 
-export const getNames = transactions => getStats(transactions).map(category => category.name)
-export const getValues = transactions => getStats(transactions).map(category => category.amount) 
-export const getColors = transactions => getStats(transactions).map(category => category.color)  
+export const getNames = transactions => getStats(transactions).map(category => category.name);
+export const getValues = transactions => getStats(transactions).map(category => category.amount);
+export const getColors = transactions => getStats(transactions).map(category => category.color);
 
 const uniqueBy = prop => list => {
-    const uniques = {}
-    return list.reduce(
-        (result, item) => {
-            if (uniques[item[prop]]) return result
-            uniques[item[prop]] = item
-            return [...result, item]
-        },
-        [],
-    )
+  const uniques = {};
+  return list.reduce((result, item) => {
+    if (uniques[item[prop]]) return result;
+    uniques[item[prop]] = item;
+    return [...result, item];
+  }, []);
 };
-
