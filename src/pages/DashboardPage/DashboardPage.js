6595f8be49a885bd09e7eadc660badc492a7b667
ViktorBaklar <baklarv@gmail.com>
2021-09-05 00:25:20 +0300

    add scroll to stats
    use react-media

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,81 +1,126 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { financeOperations } from '../../redux/finance';
+import { /* useDispatch, */ useSelector } from 'react-redux';
+// import { financeOperations } from '../../redux/finance';
 import { globalSelectors } from '../../redux/global';
-import React, { useEffect, useState } from 'react';
-import DiagramTab from '../../components/DiagramTab';
+import React, { /* useEffect, */ useState, Fragment } from 'react';
+import Media from 'react-media';
+// import DiagramTab from '../../components/DiagramTab';
 import {
   Header,
-  Navigation,
-  Currency,
-  Balance,
-  OperationsTable,
+  // Navigation,
+  // Currency,
+  // Balance,
+  // OperationsTable,
   ContainerAppWrapper,
-  AddTransactionBtn,
+  // AddTransactionBtn,
   Modal,
   AddTransactionForm,
 } from '../../components';
 // import { Container } from 'react-bootstrap';
-import { DashboardContainer } from '../../components/Container'
+// import { DashboardContainer } from '../../components/Container'
+import MobileDashboard from './MobileDashboard';
+import TabletDashboard from './TabletDashboard';
 
 import style from './Style.module.css';
 
-const DashboardPage = () => {
-  const dispatch = useDispatch();
-  // const [mainView, setMainView] = useState(true);
-  const [mainView, setMainView] = useState('main');
-  const isModalOpen = useSelector(globalSelectors.getIsModalAddTransactionOpen);
+// const DashboardPage = () => {
+//   const dispatch = useDispatch();
+//   // const [mainView, setMainView] = useState(true);
+//   const [mainView, setMainView] = useState('main');
+//   const isModalOpen = useSelector(globalSelectors.getIsModalAddTransactionOpen);
 
-  useEffect(() => {
-    dispatch(financeOperations.getTransactions());
-    dispatch(financeOperations.getCategories());
-    dispatch(financeOperations.getBalance());
-  }, [dispatch]);
+//   useEffect(() => {
+//     dispatch(financeOperations.getTransactions());
+//     dispatch(financeOperations.getCategories());
+//     dispatch(financeOperations.getBalance());
+//   }, [dispatch]);
+
+//   return (
+//     <ContainerAppWrapper>
+//       <Header viewChanger={setMainView} />
+//       <div className={style.dashboardPage}>
+//         {/* <div className={style.backdrop}> */}
+//         <Media queries={{
+//           mobile: "(max-width: 767px)",
+//           tablet: "(min-width: 768px) and (max-width: 1279px)",
+//           desktop: "(min-width: 1280px)"
+//         }}>
+//         <DashboardContainer className={style.backdrop}>
+//           <div className={style.leftContainer}>
+//             <div className={style.navAndBalanceContainer}>
+//               <div className={style.navigationContainer}>
+//                 <Navigation viewChanger={setMainView} mainView={mainView} />
+//               </div>
+
+//               {mainView === 'main' && (
+//                 <div className={style.balanceContainer}>
+//                   <Balance />
+//                 </div>
+//               )}
+//               {mainView === 'currency' && (
+//                 <div className={style.currencyContainer}>
+//                   <Currency />
+//                 </div>
+//               )}
+//             </div>
+//             <div className={style.currencyContainerTD}>
+//               <Currency />
+//             </div>
+//           </div>
+
+//           <div className={style.divider}></div>
+
+//           <div className={style.rightContainer}>
+//             {mainView === 'main' && <OperationsTable viewCondition={mainView} />}
+//             {mainView === 'stats' && <DiagramTab />}
+//           </div>
+//         </DashboardContainer>
+//         </Media>
+//         {/* </div> */}
+//       </div>
+//       {/* убрала кнопку со добавления со страницы статистики */}
+//       {mainView !== 'currency' && mainView !=='stats'&& <AddTransactionBtn />}
+//       {isModalOpen && (
+//         <Modal>
+//           <AddTransactionForm />
+//         </Modal>
+//       )}
+//     </ContainerAppWrapper>
+//   );
+// };
+
+const DashboardPage = () => {
+  // const dispatch = useDispatch();
+  // const [mainView, setMainView] = useState('main');
+  const [setMainView] = useState('main');
+  const isModalOpen = useSelector(globalSelectors.getIsModalAddTransactionOpen);
 
   return (
     <ContainerAppWrapper>
       <Header viewChanger={setMainView} />
       <div className={style.dashboardPage}>
-        {/* <div className={style.backdrop}> */}
-        <DashboardContainer className={style.backdrop}>
-          <div className={style.leftContainer}>
-            <div className={style.navAndBalanceContainer}>
-              <div className={style.navigationContainer}>
-                <Navigation viewChanger={setMainView} mainView={mainView} />
-              </div>
-              {mainView !== 'currency' && (
-                <div className={style.balanceContainer}>
-                  <Balance />
-                </div>
-              )}
-              {mainView === 'currency' && (
-                <div className={style.currencyContainer}>
-                  <Currency />
-                </div>
-              )}
-            </div>
-            <div className={style.currencyContainerTD}>
-              <Currency />
-            </div>
-          </div>
-
-          <div className={style.divider}></div>
-
-          <div className={style.rightContainer}>
-            {mainView === 'main' && <OperationsTable viewCondition={mainView} />}
-            {mainView === 'stats' && <DiagramTab />}
-          </div>
-        </DashboardContainer>
-        {/* </div> */}
+        <Media queries={{
+           mobile: "(max-width: 767px)",
+           tablet: "(min-width: 768px) and (max-width: 1279px)",
+           desktop: "(min-width: 1280px)"
+         }}>
+          {matches => (
+            <Fragment>
+              {matches.mobile && <MobileDashboard/>}
+              {matches.tablet && <TabletDashboard/>}
+              {matches.desktop && <TabletDashboard/>}
+            </Fragment>
+          )}
+        </Media>
       </div>
       {/* убрала кнопку со добавления со страницы статистики */}
-      {mainView !== 'currency' && mainView !=='stats'&& <AddTransactionBtn />}
+      {/* {mainView !== 'currency' && mainView !=='stats'&& <AddTransactionBtn />} */}
       {isModalOpen && (
         <Modal>
-          <AddTransactionForm />
+           <AddTransactionForm />
         </Modal>
       )}
     </ContainerAppWrapper>
   );
-};
+}
 
 export default DashboardPage;
